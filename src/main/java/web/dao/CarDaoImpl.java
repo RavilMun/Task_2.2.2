39660422eed183168bec313689f8ceb67ffcaae3
RavilMun <raulmunipov@gmail.com>
2023-11-28 01:09:30 +0500

    fix

--- src/main/java/web/dao/CarDaoImpl.java
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -1,11 +1,12 @@
 package web.dao;
 
+import org.springframework.stereotype.Repository;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Repository
 public class CarDaoImpl implements CarDao {
     private final List<Car> cars = new ArrayList<>() {{
         add(new Car("Tesla", 2021, 40000));
@@ -16,7 +17,7 @@ public class CarDaoImpl implements CarDao {
     }};
 
     @Override
-    public List<Car> getCars(int count) {
-        return cars.stream().limit(count).collect(Collectors.toList());
+    public List<Car> getAllCars(int count) {
+        return cars;
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,18 +1,24 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Component
+@Service
 public class CarServiceImpl implements CarService {
-    private final CarDao carDao = new CarDaoImpl();
+    private final CarDao carDao;
+
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @Override
     public List<Car> getCars(int count) {
-        return carDao.getCars(count);
+        return carDao.getAllCars(count).stream().limit(count).collect(Collectors.toList());
     }
 }
